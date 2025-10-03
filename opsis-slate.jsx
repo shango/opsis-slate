fb92cfa32ef1bd8e96c265298b1ff7f089473fd9
shango <shannon.gold@gmail.com>
2025-10-03 10:53:20 -0600

    fixed readme

--- opsis-slate.jsx
+++ opsis-slate.jsx
@@ -1,6 +1,6 @@
 /*
  * Comp Duration Manager with Slate Generator for After Effects
- * Version: 0.1.2
+ * Version: 0.1.3
  * 
  * This script creates a dockable UI panel for setting comp durations
  * and generating/updating slate frames with project information.
